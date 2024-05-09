a0cf8d191762c1956a30aefd7180cef6615535fb
Shubh-patel121 <shubham.patidar2013@gmail.com>
2024-05-09 17:47:14 +0530

    [Repo-initialization]: tic-tac-toe game

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tic-Tac-Toe-Game
 React based game application
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Tic-tac-toe game initialization
+    </div>
+  );
+}
+
+export default App;
